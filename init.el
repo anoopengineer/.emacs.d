2bd16a052f35eec9307caa197864fa19fd01106a
Anoop Engineer <anoopengineer@gmail.com>
2020-08-29 22:49:13 -0500

    feat: enabled IDO mode everywhere

--- init.el
+++ init.el
@@ -72,6 +72,15 @@
 ;; Custom key rebindings
 ;; ===================================
 (global-set-key (kbd "M-o") 'other-window)
+
+;; ===================================
+;; IDO mode customization
+;; ===================================
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+
 ;; ===================================
 ;; Ivy Customization
 ;; ===================================
